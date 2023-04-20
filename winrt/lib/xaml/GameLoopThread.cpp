8334464f64ca863be49f5510ad7bbb146f2b7f2e
cnbluefire <909683964@qq.com>
2023-04-20 08:46:44 +0800

    fix GameLoopThread cannot decontructor

--- winrt/lib/xaml/GameLoopThread.cpp
+++ winrt/lib/xaml/GameLoopThread.cpp
@@ -18,15 +18,15 @@ using namespace ABI::Microsoft::Graphics::Canvas;
 //
 static ComPtr<IDispatcherQueueController> CreateDispatcherQueueController(/*ISwapChainPanel* swapChainPanel*/)
 {
-    ComPtr<IDispatcherQueueControllerStatics> dispatcherQueueControllerStatics;
-    ThrowIfFailed(GetActivationFactory(
-        HStringReference(RuntimeClass_Microsoft_UI_Dispatching_DispatcherQueueController).Get(),
-        &dispatcherQueueControllerStatics));
+	ComPtr<IDispatcherQueueControllerStatics> dispatcherQueueControllerStatics;
+	ThrowIfFailed(GetActivationFactory(
+		HStringReference(RuntimeClass_Microsoft_UI_Dispatching_DispatcherQueueController).Get(),
+		&dispatcherQueueControllerStatics));
 
-    ComPtr<IDispatcherQueueController> dispatcherQueueController;
-    ThrowIfFailed(dispatcherQueueControllerStatics->CreateOnCurrentThread(&dispatcherQueueController));
+	ComPtr<IDispatcherQueueController> dispatcherQueueController;
+	ThrowIfFailed(dispatcherQueueControllerStatics->CreateOnCurrentThread(&dispatcherQueueController));
 
-    return dispatcherQueueController;
+	return dispatcherQueueController;
 }
 
 //
@@ -34,19 +34,19 @@ static ComPtr<IDispatcherQueueController> CreateDispatcherQueueController(/*ISwa
 //
 static ComPtr<IAsyncAction> StartThread(ComPtr<IWorkItemHandler>&& handler)
 {
-    ComPtr<IThreadPoolStatics> threadPoolStatics;
-    ThrowIfFailed(GetActivationFactory(
-        HStringReference(RuntimeClass_Windows_System_Threading_ThreadPool).Get(),
-        &threadPoolStatics));
+	ComPtr<IThreadPoolStatics> threadPoolStatics;
+	ThrowIfFailed(GetActivationFactory(
+		HStringReference(RuntimeClass_Windows_System_Threading_ThreadPool).Get(),
+		&threadPoolStatics));
 
-    ComPtr<IAsyncAction> action;
-    ThrowIfFailed(threadPoolStatics->RunWithPriorityAndOptionsAsync(
-        handler.Get(),
-        WorkItemPriority_Normal,
-        WorkItemOptions_TimeSliced,
-        &action));
-    
-    return action;        
+	ComPtr<IAsyncAction> action;
+	ThrowIfFailed(threadPoolStatics->RunWithPriorityAndOptionsAsync(
+		handler.Get(),
+		WorkItemPriority_Normal,
+		WorkItemOptions_TimeSliced,
+		&action));
+
+	return action;
 }
 
 //
@@ -54,282 +54,291 @@ static ComPtr<IAsyncAction> StartThread(ComPtr<IWorkItemHandler>&& handler)
 //
 static void SetThreadName(LPCSTR szThreadName)
 {
-    #pragma pack(push, 8)
-    struct THREADNAME_INFO
-    {
-        DWORD dwType;
-        LPCSTR szName;
-        DWORD dwThreadID;
-        DWORD dwFlags;
-    };
-    #pragma pack(pop)
+#pragma pack(push, 8)
+	struct THREADNAME_INFO
+	{
+		DWORD dwType;
+		LPCSTR szName;
+		DWORD dwThreadID;
+		DWORD dwFlags;
+	};
+#pragma pack(pop)
 
-    THREADNAME_INFO info{ 0x1000, szThreadName, 0xFFFFFFFF, 0 };
+	THREADNAME_INFO info{ 0x1000, szThreadName, 0xFFFFFFFF, 0 };
 
-    __try
-    {
-        RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), reinterpret_cast<ULONG_PTR*>(&info));
-    }
-    __except(EXCEPTION_CONTINUE_EXECUTION) { }
+	__try
+	{
+		RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), reinterpret_cast<ULONG_PTR*>(&info));
+	}
+	__except (EXCEPTION_CONTINUE_EXECUTION) {}
 }
 
 //
 // The implementation of IGameLoopThread.
 //
 class GameLoopThread : public IGameLoopThread
-                     , private LifespanTracker<GameLoopThread>
+	, private LifespanTracker<GameLoopThread>
 {
-    ICanvasGameLoopClient* m_client;
-    std::mutex m_mutex;
-    std::condition_variable m_conditionVariable;
+	ICanvasGameLoopClient* m_client;
+	std::mutex m_mutex;
+	std::condition_variable m_conditionVariable;
 
-    ComPtr<IDispatcherQueueController> m_dispatcherQueueController;
-    ComPtr<IDispatcherQueue> m_dispatcherQueue;
-    
-    bool m_started;
-    bool m_startDispatcher;
-    bool m_dispatcherStarted;
-    bool m_shutdownRequested;
-    bool m_shutdown;
-    std::vector<ComPtr<AnimatedControlAsyncAction>> m_pendingActions;
+	ComPtr<IDispatcherQueueController> m_dispatcherQueueController;
+	ComPtr<IDispatcherQueue> m_dispatcherQueue;
+
+	bool m_started;
+	bool m_startDispatcher;
+	bool m_dispatcherStarted;
+	bool m_shutdownRequested;
+	bool m_shutdown;
+	std::vector<ComPtr<AnimatedControlAsyncAction>> m_pendingActions;
 
 public:
-    GameLoopThread(ComPtr<ISwapChainPanel> swapChainPanel, ICanvasGameLoopClient* client)
-        : m_client(client)
-        , m_started(false)
-        , m_startDispatcher(false)
-        , m_dispatcherStarted(false)
-        , m_shutdownRequested(false)
-        , m_shutdown(false)
-    {
-        auto threadHandler = Callback<AddFtmBase<IWorkItemHandler>::Type>(
-            [=] (IAsyncAction*)
-            {
-                return ExceptionBoundary([&] { ThreadMain(swapChainPanel); });
-            });
-        auto action = StartThread(std::move(threadHandler));
+	GameLoopThread(ComPtr<ISwapChainPanel> swapChainPanel, ICanvasGameLoopClient* client)
+		: m_client(client)
+		, m_started(false)
+		, m_startDispatcher(false)
+		, m_dispatcherStarted(false)
+		, m_shutdownRequested(false)
+		, m_shutdown(false)
+	{
+		auto threadHandler = Callback<AddFtmBase<IWorkItemHandler>::Type>(
+			[=](IAsyncAction*)
+			{
+				return ExceptionBoundary([&] { ThreadMain(swapChainPanel); });
+			});
+		auto action = StartThread(std::move(threadHandler));
 
-        auto threadCompletedHandler = Callback<AddFtmBase<IAsyncActionCompletedHandler>::Type>(
-            [=] (IAsyncAction*, AsyncStatus)
-            {
-                return ExceptionBoundary([&] { ThreadCompleted(); });
-            });
+		auto threadCompletedHandler = Callback<AddFtmBase<IAsyncActionCompletedHandler>::Type>(
+			[=](IAsyncAction*, AsyncStatus)
+			{
+				return ExceptionBoundary([&] { ThreadCompleted(); });
+			});
 
-        ThrowIfFailed(action->put_Completed(threadCompletedHandler.Get()));
+		ThrowIfFailed(action->put_Completed(threadCompletedHandler.Get()));
 
-        auto lock = GetLock();
-        m_conditionVariable.wait(lock, [=] { return m_started || m_shutdown; });
-    }
+		auto lock = GetLock();
+		m_conditionVariable.wait(lock, [=] { return m_started || m_shutdown; });
+	}
 
-    ~GameLoopThread()
-    {
-        auto lock = GetLock();
-        
-        if (m_dispatcherQueueController)
-        {
-            ComPtr<IAsyncAction> asyncAction;
-            m_dispatcherQueueController->ShutdownQueueAsync(&asyncAction);
-            // Do I need to do something with async action here?
-        }
+	~GameLoopThread()
+	{
+		auto lock = GetLock();
 
-        m_shutdownRequested = true;
-        m_conditionVariable.notify_all();
+		if (m_dispatcherQueueController)
+		{
+			ComPtr<IAsyncAction> asyncAction;
+			m_dispatcherQueueController->ShutdownQueueAsync(&asyncAction);
 
-        m_conditionVariable.wait(lock, [=] { return m_shutdown; });
-    }
+			auto shutdownCompletedHandler = Callback<AddFtmBase<IAsyncActionCompletedHandler>::Type>(
+				[=](IAsyncAction*, AsyncStatus)
+				{					
+					return ExceptionBoundary([&] { ::PostQuitMessage(0); });
+				});
 
-    virtual void StartDispatcher() override
-    {
-        auto lock = GetLock();
-        m_startDispatcher = true;
-        m_conditionVariable.notify_all();
-    }
+			ThrowIfFailed(asyncAction->put_Completed(shutdownCompletedHandler.Get()));
 
-    virtual void StopDispatcher() override
-    {
-        auto lock = GetLock();
+			// Do I need to do something with async action here?
+		}
 
-        m_startDispatcher = false;
+		m_shutdownRequested = true;
+		m_conditionVariable.notify_all();
 
-        if (m_dispatcherStarted)
-        {
-            assert(m_dispatcherQueue);
+		m_conditionVariable.wait(lock, [=] { return m_shutdown; });
+	}
 
-            auto callback = Callback<AddFtmBase<IDispatcherQueueHandler>::Type>([]() {
-                ::PostQuitMessage(0);
-                return S_OK;
-                });
+	virtual void StartDispatcher() override
+	{
+		auto lock = GetLock();
+		m_startDispatcher = true;
+		m_conditionVariable.notify_all();
+	}
 
-            boolean result;
-            ThrowIfFailed(m_dispatcherQueue->TryEnqueueWithPriority(DispatcherQueuePriority_Low, callback.Get(), &result));
-        }
+	virtual void StopDispatcher() override
+	{
+		auto lock = GetLock();
 
-        m_conditionVariable.notify_all();
-        m_conditionVariable.wait(lock, [=] { return !m_dispatcherStarted; });
-    }
+		m_startDispatcher = false;
 
-    virtual ComPtr<IAsyncAction> RunAsync(IDispatcherQueueHandler* handler) override
-    {
-        auto lock = GetLock();
+		if (m_dispatcherStarted)
+		{
+			assert(m_dispatcherQueue);
 
-        auto action = Make<AnimatedControlAsyncAction>(handler);
+			auto callback = Callback<AddFtmBase<IDispatcherQueueHandler>::Type>([]() {
+				::PostQuitMessage(0);
+				return S_OK;
+				});
 
-        if (m_startDispatcher)
-        {
-            auto callback = Callback<AddFtmBase<IDispatcherQueueHandler>::Type>(
-                [action]() {
-                    return action->InvokeAndFireCompletion().ActionResult;
-                });
+			boolean result;
+			ThrowIfFailed(m_dispatcherQueue->TryEnqueueWithPriority(DispatcherQueuePriority_Low, callback.Get(), &result));
+		}
 
-            boolean result;
-            HRESULT hr = m_dispatcherQueue->TryEnqueueWithPriority(DispatcherQueuePriority_Low, callback.Get(), &result);
+		m_conditionVariable.notify_all();
+		m_conditionVariable.wait(lock, [=] { return !m_dispatcherStarted; });
+	}
 
-            //// Work around MSFT:8381339. CoreDispatcher.RunAsync can fail with E_INVALIDARG
-            //// when an internal counter wraps. We can recover from this by a simple retry.
-            //if (hr == E_INVALIDARG)
-            //{
-            //    hr = m_dispatcher->RunAsync(CoreDispatcherPriority_Low, handler, &action);
-            //}
+	virtual ComPtr<IAsyncAction> RunAsync(IDispatcherQueueHandler* handler) override
+	{
+		auto lock = GetLock();
 
-            ThrowIfFailed(hr);
-            return action;
-        }
-        else
-        {
-            m_pendingActions.push_back(action);
-            m_conditionVariable.notify_all();
-            return action;
-        }
-    }
+		auto action = Make<AnimatedControlAsyncAction>(handler);
 
-    virtual bool HasThreadAccess() override
-    {
-        auto lock = GetLock();
+		if (m_startDispatcher)
+		{
+			auto callback = Callback<AddFtmBase<IDispatcherQueueHandler>::Type>(
+				[action]() {
+					return action->InvokeAndFireCompletion().ActionResult;
+				});
 
-        if (m_dispatcherQueue)
-        {
-            boolean result;
-            ThrowIfFailed(As<IDispatcherQueue2>(m_dispatcherQueue)->get_HasThreadAccess(&result));
+			boolean result;
+			HRESULT hr = m_dispatcherQueue->TryEnqueueWithPriority(DispatcherQueuePriority_Low, callback.Get(), &result);
 
-            
-            return !!result;
-        }
-        else
-        {
-            return false;
-        }
-    }
+			//// Work around MSFT:8381339. CoreDispatcher.RunAsync can fail with E_INVALIDARG
+			//// when an internal counter wraps. We can recover from this by a simple retry.
+			//if (hr == E_INVALIDARG)
+			//{
+			//    hr = m_dispatcher->RunAsync(CoreDispatcherPriority_Low, handler, &action);
+			//}
+
+			ThrowIfFailed(hr);
+			return action;
+		}
+		else
+		{
+			m_pendingActions.push_back(action);
+			m_conditionVariable.notify_all();
+			return action;
+		}
+	}
+
+	virtual bool HasThreadAccess() override
+	{
+		auto lock = GetLock();
+
+		if (m_dispatcherQueue)
+		{
+			boolean result;
+			ThrowIfFailed(As<IDispatcherQueue2>(m_dispatcherQueue)->get_HasThreadAccess(&result));
+
+
+			return !!result;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 private:
-    Lock GetLock()
-    {
-        return Lock(m_mutex);
-    }
+	Lock GetLock()
+	{
+		return Lock(m_mutex);
+	}
 
-    void ThreadMain(ComPtr<ISwapChainPanel> swapChainPanel)
-    {
-        SetThreadName(Strings::GameLoopThreadName);
+	void ThreadMain(ComPtr<ISwapChainPanel> swapChainPanel)
+	{
+		SetThreadName(Strings::GameLoopThreadName);
 
-        auto lock = GetLock();
+		auto lock = GetLock();
 
-        m_dispatcherQueueController = CreateDispatcherQueueController();
-        ThrowIfFailed(m_dispatcherQueueController->get_DispatcherQueue(&m_dispatcherQueue));
-        swapChainPanel.Reset(); // we only needed this to create the dispatcher
-        m_conditionVariable.notify_all();
+		m_dispatcherQueueController = CreateDispatcherQueueController();
+		ThrowIfFailed(m_dispatcherQueueController->get_DispatcherQueue(&m_dispatcherQueue));
+		swapChainPanel.Reset(); // we only needed this to create the dispatcher
+		m_conditionVariable.notify_all();
 
-        lock.unlock();
-        m_client->OnGameLoopStarting();
-        lock.lock();
+		lock.unlock();
+		m_client->OnGameLoopStarting();
+		lock.lock();
 
-        m_started = true;
-        m_conditionVariable.notify_all();        
+		m_started = true;
+		m_conditionVariable.notify_all();
 
-        for (;;)
-        {
-            m_conditionVariable.wait(lock, [=] { return m_shutdownRequested || !m_pendingActions.empty() || m_startDispatcher; });
+		for (;;)
+		{
+			m_conditionVariable.wait(lock, [=] { return m_shutdownRequested || !m_pendingActions.empty() || m_startDispatcher; });
 
-            if (m_shutdownRequested)
-                break;
+			if (m_shutdownRequested)
+				break;
 
-            if (!m_pendingActions.empty())
-            {
-                std::vector<ComPtr<AnimatedControlAsyncAction>> actions;
-                std::swap(actions, m_pendingActions);
-                
-                lock.unlock();
-                RunActions(std::move(actions));
-                lock.lock();
-            }
-            else if (m_startDispatcher)
-            {
-                m_dispatcherStarted = true;
-                m_conditionVariable.notify_all();
-                
-                lock.unlock();
+			if (!m_pendingActions.empty())
+			{
+				std::vector<ComPtr<AnimatedControlAsyncAction>> actions;
+				std::swap(actions, m_pendingActions);
 
-                MSG msg{};
-                while (GetMessage(&msg, NULL, 0, 0))
-                {
-                    DispatchMessage(&msg);
-                }
-                //ThrowIfFailed(m_dispatcher->ProcessEvents(CoreProcessEventsOption_ProcessUntilQuit));
-                
-                lock.lock();
+				lock.unlock();
+				RunActions(std::move(actions));
+				lock.lock();
+			}
+			else if (m_startDispatcher)
+			{
+				m_dispatcherStarted = true;
+				m_conditionVariable.notify_all();
 
-                m_dispatcherStarted = false;
-                m_conditionVariable.notify_all();
-            }
-        }
+				lock.unlock();
 
-        // Cancel any remaining actions
-        CancelActions(lock);
+				MSG msg{};
+				while (GetMessage(&msg, NULL, 0, 0))
+				{
+					DispatchMessage(&msg);
+				}
+				//ThrowIfFailed(m_dispatcher->ProcessEvents(CoreProcessEventsOption_ProcessUntilQuit));
 
-        lock.unlock();
-        m_client->OnGameLoopStopped();
+				lock.lock();
 
-        // falling out of ThreadMain will cause ThreadCompleted to be called,
-        // which will mark the thread as shutdown.
-    }
+				m_dispatcherStarted = false;
+				m_conditionVariable.notify_all();
+			}
+		}
 
-    void ThreadCompleted()
-    {
-        // This method is called whenever the thread action completes (even if
-        // it threw an exception).
+		// Cancel any remaining actions
+		CancelActions(lock);
 
-        auto lock = GetLock();
-        m_shutdown = true;
-        m_conditionVariable.notify_all();        
-        // as soon as the lock is released the destructor might run, so 'this'
-        // is essentially invalid now.
-    }
+		lock.unlock();
+		m_client->OnGameLoopStopped();
 
-    void RunActions(std::vector<ComPtr<AnimatedControlAsyncAction>>&& actions)
-    {
-        for (auto& action : actions)
-        {
-            action->InvokeAndFireCompletion();
-        }
+		// falling out of ThreadMain will cause ThreadCompleted to be called,
+		// which will mark the thread as shutdown.
+	}
 
-        actions.clear();
-    }
+	void ThreadCompleted()
+	{
+		// This method is called whenever the thread action completes (even if
+		// it threw an exception).
 
-    void CancelActions(Lock const& lock)
-    {
-        MustOwnLock(lock);
+		auto lock = GetLock();
+		m_shutdown = true;
+		m_conditionVariable.notify_all();
+		// as soon as the lock is released the destructor might run, so 'this'
+		// is essentially invalid now.
+	}
 
-        for (auto& action : m_pendingActions)
-        {
-            action->CancelAndFireCompletion();
-        }
+	void RunActions(std::vector<ComPtr<AnimatedControlAsyncAction>>&& actions)
+	{
+		for (auto& action : actions)
+		{
+			action->InvokeAndFireCompletion();
+		}
 
-        m_pendingActions.clear();
-    }
+		actions.clear();
+	}
+
+	void CancelActions(Lock const& lock)
+	{
+		MustOwnLock(lock);
+
+		for (auto& action : m_pendingActions)
+		{
+			action->CancelAndFireCompletion();
+		}
+
+		m_pendingActions.clear();
+	}
 };
 
 
 std::unique_ptr<IGameLoopThread> CreateGameLoopThread(ISwapChainPanel* swapChainPanel, ICanvasGameLoopClient* client)
 {
-    return std::make_unique<GameLoopThread>(swapChainPanel, client);
+	return std::make_unique<GameLoopThread>(swapChainPanel, client);
 }
 
 #endif
